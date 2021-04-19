8d442c29c26417f3b2f071ffd5cb73e4145c1d04
sharmakirtan <82703377+sharmakirtan@users.noreply.github.com>
2021-04-19 10:49:55 +0530

    Update VehicleLicense2Application.java

--- src/main/java/com/capgemini/VehicleLicense2Application.java
+++ src/main/java/com/capgemini/VehicleLicense2Application.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 //import springfox.documentation.spi.DocumentationType;
 //import springfox.documentation.spring.web.plugins.Docket;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//@Configuration
 //@EnableSwagger2
 @SpringBootApplication
 public class VehicleLicense2Application {
